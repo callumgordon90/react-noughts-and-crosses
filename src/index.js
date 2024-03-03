efdc85a9baffb6f8bb3d0764858ce8893c266346
callum <callumjosephiaingordon@gmail.com>
2024-03-03 11:07:48 +0100

    deleted unecessary template files and configured environment to begin project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import App from './App';
-import './index.css';
+import "./styles.css";
+//import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
+
+
+/////////////////////////////////////77/////////
